5cda4017dc1ac3edc6364d9e72600086444530e6
max190708 <yelisieiev.maksym.studente@itispaleocapa.it>
2026-05-08 10:34:32 +0200

    struct creata

---
+++ Numerazione civica.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+struct Numerazione civica{
+string 	classeTopinimo;
+string descrizioneToponimo;
+int numero;
+string subalterno;
+int cap;
+int sezioneistat;
+double lat;
+double lon;
+double location;
+};	
+	
+	
+int main(){
+
+	
+	
+	
+	
+	return 0;
+}
